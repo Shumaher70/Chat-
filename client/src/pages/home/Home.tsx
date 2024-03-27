e2116cdd7cce2e18ed0db4a107b161d0108eee13
oleg <olegvraby@gmail.com>
2024-03-27 19:13:18 +0200

    add:home/Home.tsx: added styles and context to page

--- client/src/pages/home/Home.tsx
+++ client/src/pages/home/Home.tsx
@@ -1,7 +1,24 @@
+import styles from './Home.module.scss';
+import ButtonGray from '../../components/ButtonGray';
+
+import image from '../../images/home/spiral.webp';
+
 const Home = () => {
    return (
-      <div>
-         <h1>Home page</h1>
+      <div className={styles.container}>
+         <img src={image} alt="spiral" />
+
+         <h1 className="hero">Connect to chat</h1>
+
+         <div>
+            <ButtonGray variant="contained" size="large">
+               sing in
+            </ButtonGray>
+
+            <ButtonGray variant="contained" size="large">
+               sing out
+            </ButtonGray>
+         </div>
       </div>
    );
 };
