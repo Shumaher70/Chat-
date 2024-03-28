b99aec6c941ea52efb0b9bdefbab7716beea2223
oleg <olegvraby@gmail.com>
2024-03-28 14:25:05 +0200

    add:App.tsx:added route for "NotFoundPage"

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,12 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/home/Home';
+import NotFound from './pages/notFound/NotFound';
 
 function App() {
    return (
       <BrowserRouter>
          <Routes>
+            <Route path="*" element={<NotFound />} />
             <Route path="/" element={<Home />} />
          </Routes>
       </BrowserRouter>
