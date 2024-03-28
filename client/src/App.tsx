9080870cac119f7467c74da4c99ed173549720eb
oleg <olegvraby@gmail.com>
2024-03-28 18:16:46 +0200

    move:Home: moved auth to LoginPage

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/home/Home';
 import NotFound from './pages/notFound/NotFound';
+import LoginPage from './auth/LoginPage';
 
 function App() {
    return (
@@ -8,6 +9,7 @@ function App() {
          <Routes>
             <Route path="*" element={<NotFound />} />
             <Route path="/" element={<Home />} />
+            <Route path="/login" element={<LoginPage />} />
          </Routes>
       </BrowserRouter>
    );
--- client/src/pages/home/Home.tsx
+++ client/src/pages/home/Home.tsx
@@ -2,42 +2,12 @@ import styles from './Home.module.scss';
 import ButtonGray from '../../components/ButtonGray';
 
 import { useNavigate } from 'react-router-dom';
-import { Session, createClient } from '@supabase/supabase-js';
-import { useEffect, useState } from 'react';
-import { ThemeSupa } from '@supabase/auth-ui-shared';
-import { Auth } from '@supabase/auth-ui-react';
-import CloseIcon from '@mui/icons-material/Close';
-
-const supabase = createClient(
-   process.env.REACT_APP_SUPERBASE_URL as string,
-   process.env.REACT_APP_SUPEBASE_PUBLICK_KEY as string
-);
 
 const Home = () => {
-   const [session, setSession] = useState<Session | null>(null);
-   const [showAuth, setShowAuth] = useState<boolean>(false);
    const navigate = useNavigate();
 
-   useEffect(() => {
-      if (session) {
-         navigate('/chat');
-      }
-
-      supabase.auth.getSession().then(({ data: { session } }) => {
-         setSession(session);
-      });
-
-      const {
-         data: { subscription },
-      } = supabase.auth.onAuthStateChange((_event, session) => {
-         setSession(session);
-      });
-
-      return () => subscription.unsubscribe();
-   }, [navigate, session]);
-
    const handleClick = () => {
-      setShowAuth((previous) => !previous);
+      navigate('/login');
    };
 
    return (
@@ -55,31 +25,6 @@ const Home = () => {
                </ButtonGray>
             </div>
          </div>
-
-         {showAuth && (
-            <div className={styles.auth}>
-               <Auth
-                  supabaseClient={supabase}
-                  appearance={{
-                     theme: ThemeSupa,
-                     style: {
-                        container: {
-                           backgroundColor: 'white',
-                           margin: 0,
-                           padding: 10,
-                           minWidth: 300,
-                        },
-                        divider: {
-                           margin: 0,
-                        },
-                     },
-                  }}
-                  providers={['google', 'github']}
-               />
-
-               <CloseIcon onClick={handleClick} className={styles.close} />
-            </div>
-         )}
       </>
    );
 };
