fedea11093acaf5daf6249105ebcaf29d2008970
oleg <olegvraby@gmail.com>
2024-03-29 12:20:35 +0200

    add:App added authProvide

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,14 +2,18 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/home/Home';
 import NotFound from './pages/notFound/NotFound';
 import LoginPage from './auth/LoginPage';
+import AuthLayout from './auth/AuthLayout';
 
 function App() {
    return (
       <BrowserRouter>
          <Routes>
             <Route path="*" element={<NotFound />} />
-            <Route path="/" element={<Home />} />
-            <Route path="/login" element={<LoginPage />} />
+            <Route element={<AuthLayout />}>
+               <Route path="/" element={<Home />} />
+               <Route path="/login" element={<LoginPage />} />
+               <Route path="/chat" element={<div>chat</div>} />
+            </Route>
          </Routes>
       </BrowserRouter>
    );
