d73422ad58bdc8192e0710bd53dded9e12345ac8
oleg <olegvraby@gmail.com>
2024-04-02 14:43:47 +0200

    add:TextInput: added input and some style

--- client/src/pages/chat/components/textInput/TextInput.tsx
+++ client/src/pages/chat/components/textInput/TextInput.tsx
@@ -1,6 +1,20 @@
+import { TextField } from '@mui/material';
 import styles from './TextInput.module.scss';
+import useCustomTextInputTheme from './useCustomTextInputTheme';
 const TextInput = () => {
-   return <div className={`${styles.textInput} dark-2`}>TextInput</div>;
+   const { theme } = useCustomTextInputTheme();
+   return (
+      <div className={`${styles.textInput} dark-2`}>
+         <div className={`${styles.inputContainer}`}>
+            <TextField
+               id="outlined-basic"
+               label="Enter your message"
+               variant="filled"
+               sx={theme}
+            />
+         </div>
+      </div>
+   );
 };
 
 export default TextInput;
