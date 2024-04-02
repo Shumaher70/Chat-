9a992073b8f3823ee1f834620d4756bb5d4e6aa6
oleg <olegvraby@gmail.com>
2024-04-02 20:40:24 +0200

    add:Chat.tsx:the added observer has been added and combined with the trigger function

--- client/src/pages/chat/Chat.tsx
+++ client/src/pages/chat/Chat.tsx
@@ -3,11 +3,26 @@ import Messages from './components/messages/Messages';
 import TextInput from './components/textInput/TextInput';
 
 import styles from './Chat.module.scss';
+import useObserver from '../../hooks/useObserver';
+import { useAppSelector } from '../../redux/hooks/hooks';
+import { dashboardTrigger } from './utils/dashboardTrigger';
 
 const Chat = () => {
+   const [width] = useObserver();
+   const dashboardTriggerSlice = useAppSelector(
+      (state) => state.dashboardReducer.trigger
+   );
+
+   const trigger: string = dashboardTrigger(
+      width,
+      dashboardTriggerSlice,
+      styles.dashboardContainerHiding,
+      styles.dashboardContainerShowing
+   );
+
    return (
       <main className={styles.chat}>
-         <div className={styles.dashboardContainer}>
+         <div className={`${styles.dashboardContainer} ${trigger}`}>
             <Dashboard />
          </div>
          <div className={styles.message_textInput_container}>
