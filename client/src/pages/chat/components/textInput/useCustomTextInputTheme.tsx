e7ed60c19ee64868e91e6f27b7446990eb5f9af8
oleg <olegvraby@gmail.com>
2024-04-02 14:44:05 +0200

    created: useCustomTextInputTheme.tsx

---
+++ client/src/pages/chat/components/textInput/useCustomTextInputTheme.tsx
@@ -0,0 +1,34 @@
+import { useAppSelector } from '../../../../redux/hooks/hooks';
+
+const useCustomTextInputTheme = () => {
+   const themeSlice = useAppSelector((state) => state.themeReducer.theme);
+   const theme = {
+      width: '100%',
+      '& .MuiFilledInput-root': {
+         color: `${themeSlice === 'light' ? '#0f0f0f' : '#f2f2f2'}`,
+         backgroundColor: `${themeSlice === 'light' ? '#C8C8C8' : '#434145'}`,
+         borderRadius: '7px',
+         overflow: 'hidden',
+         '&:before': {
+            borderColor: '#2e2e2e',
+            borderWidth: '0px',
+         },
+         '&:after': {
+            borderColor: '#434145',
+            borderWidth: '3px',
+         },
+      },
+      '& .MuiInputLabel-filled': {
+         color: `${themeSlice === 'light' ? '#0f0f0f' : '#f2f2f2'}`,
+         '&.Mui-focused': {
+            color: `${themeSlice === 'light' ? '#0f0f0f' : '#f2f2f2'}`,
+         },
+      },
+   };
+
+   return {
+      theme,
+   };
+};
+
+export default useCustomTextInputTheme;
