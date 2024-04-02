896d6dc13197aa58cfc4a4029fff14104b094788
oleg <olegvraby@gmail.com>
2024-04-02 12:16:15 +0200

    change:DashBoard: moved the room lists to a separate "Rooms" component

--- client/src/pages/chat/components/dashboard/Dashboard.tsx
+++ client/src/pages/chat/components/dashboard/Dashboard.tsx
@@ -1,37 +1,12 @@
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
-import ListItemButton from '@mui/material/ListItemButton';
-import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-
-import { FaReact } from 'react-icons/fa';
-import { FaNodeJs } from 'react-icons/fa';
-
 import styles from './DashBoard.module.scss';
 import Logo from './components/logo/Logo';
+import Rooms from './components/rooms/Rooms';
+
 const Dashboard = () => {
    return (
       <div className={`${styles.dashboard} dark-2`}>
          <Logo />
-
-         <List>
-            <ListItem disablePadding>
-               <ListItemButton>
-                  <ListItemIcon>
-                     <FaReact className={styles.FaReact} />
-                  </ListItemIcon>
-                  <ListItemText primary="React" />
-               </ListItemButton>
-            </ListItem>
-            <ListItem disablePadding>
-               <ListItemButton>
-                  <ListItemIcon>
-                     <FaNodeJs className={styles.FaNodeJs} />
-                  </ListItemIcon>
-                  <ListItemText primary="Node" />
-               </ListItemButton>
-            </ListItem>
-         </List>
+         <Rooms />
       </div>
    );
 };
