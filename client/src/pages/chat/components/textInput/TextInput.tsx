8f1514872e466f440e1765cbdd9032871e9599df
oleg <olegvraby@gmail.com>
2024-04-03 21:09:52 +0200

    add:TextInput.ts: 1.the textarea has been added instead of input

--- client/src/pages/chat/components/textInput/TextInput.tsx
+++ client/src/pages/chat/components/textInput/TextInput.tsx
@@ -5,6 +5,7 @@ import { useState } from 'react';
 import Enter from './components/enter/Enter';
 const TextInput = () => {
    const [input, setInput] = useState<string>('');
+   const [shiftKey, setShiftKey] = useState<boolean>(false);
    const { theme } = useCustomTextInputTheme();
 
    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -19,14 +20,21 @@ const TextInput = () => {
       <div className={`${styles.textInput} dark-2`}>
          <div className={`${styles.inputContainer}`}>
             <TextField
-               id="outlined-basic"
+               multiline
+               maxRows={4}
+               minRows={2}
                label="Enter your message"
                variant="filled"
                sx={theme}
                onChange={handleChange}
                value={input}
+               onKeyDown={(e) => setShiftKey(e.shiftKey)}
             />
-            <Enter input={input} handleClearInput={handleClearInput} />
+            <Enter
+               input={input}
+               handleClearInput={handleClearInput}
+               shiftKey={shiftKey}
+            />
          </div>
       </div>
    );
