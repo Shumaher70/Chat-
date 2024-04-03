57c9193a046477b23c597612e27d04b60f3d97df
oleg <olegvraby@gmail.com>
2024-04-03 19:20:49 +0200

    add:Message: 1.UserMessage & UsersMessages have been added to component 2.the style for scroll has been added

--- client/src/pages/chat/components/messages/Messages.tsx
+++ client/src/pages/chat/components/messages/Messages.tsx
@@ -1,10 +1,17 @@
 import LightMode from '../../../../components/lightMode/LightMode';
 import styles from './Messages.module.scss';
+import UserMessage from './components/userMessage/UserMessage';
+import UsersMessages from './components/usersMessages/UsersMessages';
+
 const Messages = () => {
    return (
-      <div className={styles.messages}>
+      <div className={styles.messagesContainer}>
          <div className={styles.lightMode}>
             <LightMode />
+         </div>
+         <div className={styles.messages}>
+            <UserMessage />
+            <UsersMessages />
          </div>
       </div>
    );
