2d5a89aae8ea025993d4f5d54c123934ba6f4c52
oleg <olegvraby@gmail.com>
2024-04-03 14:59:06 +0200

    add:TextInput.tsx: added logic for input

--- client/src/pages/chat/components/textInput/TextInput.tsx
+++ client/src/pages/chat/components/textInput/TextInput.tsx
@@ -1,8 +1,20 @@
 import { TextField } from '@mui/material';
 import styles from './TextInput.module.scss';
 import useCustomTextInputTheme from './useCustomTextInputTheme';
+import { useState } from 'react';
+import Enter from './components/enter/Enter';
 const TextInput = () => {
+   const [input, setInput] = useState<string>('');
    const { theme } = useCustomTextInputTheme();
+
+   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+      setInput(event.target.value);
+   };
+
+   const handleClearInput = () => {
+      setInput('');
+   };
+
    return (
       <div className={`${styles.textInput} dark-2`}>
          <div className={`${styles.inputContainer}`}>
@@ -11,7 +23,10 @@ const TextInput = () => {
                label="Enter your message"
                variant="filled"
                sx={theme}
+               onChange={handleChange}
+               value={input}
             />
+            <Enter input={input} handleClearInput={handleClearInput} />
          </div>
       </div>
    );
