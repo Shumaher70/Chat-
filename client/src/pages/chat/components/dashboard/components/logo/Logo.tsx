a6bc5483cd6b694afdfa4eff5673fa2a33d83fa5
oleg <olegvraby@gmail.com>
2024-04-05 19:59:57 +0200

    add:Logo.tsx: back action

--- client/src/pages/chat/components/dashboard/components/logo/Logo.tsx
+++ client/src/pages/chat/components/dashboard/components/logo/Logo.tsx
@@ -1,10 +1,31 @@
-import styles from './Logo.module.scss';
+import {
+   useAppDispatch,
+   useAppSelector,
+} from '../../../../../../redux/hooks/hooks';
+
 import BackButton from './backButton/BackButton';
+
 import UserIcon from './userIcon/UserIcon';
+import { RiArrowGoBackLine } from 'react-icons/ri';
+
+import styles from './Logo.module.scss';
+import { changeRoomAction } from '../../../../../../redux/slices/roomsSlice';
 const Logo = () => {
+   const { trigger, room } = useAppSelector((state) => state.roomReducer);
+
+   const dispatch = useAppDispatch();
+
+   const handleClick = () => {
+      dispatch(changeRoomAction(false));
+   };
+
    return (
       <div className={`${styles.logoContainer} dark-1`}>
-         <p className={styles.logo}>Chat-</p>
+         {!trigger && <p className={styles.logo}>Chat-</p>}
+         <div className={styles.backContainer} onClick={handleClick}>
+            {trigger && <p className={styles.logo}>{room}</p>}
+            {trigger && <RiArrowGoBackLine className={styles.back} />}
+         </div>
          <UserIcon />
          <BackButton />
       </div>
