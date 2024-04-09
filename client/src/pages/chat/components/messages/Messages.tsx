c45be6f522056907b6ce73d29b5badfd14b4662f
oleg <olegvraby@gmail.com>
2024-04-09 20:37:51 +0200

    move:Messages: A title has been moved to the Header component

--- client/src/pages/chat/components/messages/Messages.tsx
+++ client/src/pages/chat/components/messages/Messages.tsx
@@ -1,24 +1,10 @@
-import LightMode from '../../../../components/lightMode/LightMode';
-import { useAppSelector } from '../../../../redux/hooks/hooks';
 import styles from './Messages.module.scss';
 import UserMessage from './components/userMessage/UserMessage';
 import UsersMessages from './components/usersMessages/UsersMessages';
 
 const Messages = () => {
-   const { room, trigger } = useAppSelector((state) => state.roomReducer);
-
    return (
       <div className={styles.messagesContainer}>
-         <h1
-            className={`${styles.roomName} ${
-               trigger ? styles.showing : styles.hidden
-            }`}
-         >
-            {room}
-         </h1>
-         <div className={styles.lightMode}>
-            <LightMode />
-         </div>
          <div className={styles.messages}>
             <UsersMessages>users</UsersMessages>
             <UserMessage>user</UserMessage>
