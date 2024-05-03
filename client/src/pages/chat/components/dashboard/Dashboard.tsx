2406ffb062d728f1d43cae0ef9a27e6c89828dc7
oleg <olegvraby@gmail.com>
2024-05-03 17:21:08 +0200

    add:Dashboard: 01.the CreateRoom component has been added 02. styles have been refactoring

--- client/src/pages/chat/components/dashboard/Dashboard.tsx
+++ client/src/pages/chat/components/dashboard/Dashboard.tsx
@@ -1,12 +1,15 @@
 import { useAppSelector } from '../../../../redux/hooks/hooks';
 import styles from './DashBoard.module.scss';
+import CreateRoom from './components/createRoom/CreateRoom';
 import Logo from './components/logo/Logo';
 import Rooms from './components/rooms/Rooms';
 import Setting from './components/setting/Setting';
 import SignOut from './components/signOut/SignOut';
 
 const Dashboard = () => {
-   const { setting } = useAppSelector((state) => state.dashboardReducer);
+   const { setting, createRoom } = useAppSelector(
+      (state) => state.dashboardReducer
+   );
 
    return (
       <div className={`${styles.dashboard} dark-2`}>
@@ -21,6 +24,14 @@ const Dashboard = () => {
             <div className={`${styles.signout}`}>
                <SignOut />
             </div>
+         </div>
+
+         <div
+            className={`${styles.createRoom} ${
+               createRoom ? styles.hiddenOut : styles.hiddenIn
+            } dark-2`}
+         >
+            <CreateRoom />
          </div>
       </div>
    );
